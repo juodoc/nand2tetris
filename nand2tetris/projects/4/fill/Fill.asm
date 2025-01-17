83d00c118ff631590492cabc333db699b11cc90c
juodoc <aronjulio8@gmail.com>
2025-01-16 18:43:57 -0700

    Completed assignment fill

--- nand2tetris/projects/4/fill/Fill.asm
+++ nand2tetris/projects/4/fill/Fill.asm
@@ -8,52 +8,86 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-    @SCREEN
-    D=A
-    @addr
-    M=D
-
-    @i
-    M=0
-    @j
-    M=0 //Indices for columns and
-
     @24576
     D=A
     @key
     M=D
 
-(INIT)
-    @511
+(INIT) //initializes the screen variables
+    @SCREEN
+    D=A
+    @addr
+    M=D
+    @inner
+    M=D
+    
+    @15
     M=A
-    @255
+    @31
     M=A
 
-(COL)
+    @i //Indecis for cols rows and inner addresses per row
+    M=0
     @j
-    D=M
-    @511
-    D=M-D
+    M=0
+    @b
+    M=0
 
     @LOOP
-    D;JGT
+    0;JMP
+
+(COL) //Creates a blank screen
+    @j
+    D=M
+    @15
+    D=M-D
+
+    @INIT //returns to key listener once done
+    D;JLT
 
     @j
     M=M+1
     @i
     M=0
-(ROW)
+(ROW) //nested loop
     @i
     D=M
-    @255
-    D=D-M
+    @15
+    D=M-D
 
     @COL
-    D;JGT
+    D;JLT
 
     @addr
+    D=M
+    @inner
+    M=D
+
+    @b
+    M=0
+(INLOOP)
+    @b
+    D=M
+    @31
+    D=M-D
+
+    @ROWCONT
+    D;JLT
+
+    @inner
     A=M
     M=0
+
+    @inner
+    M=M+1
+
+    @b
+    M=M+1
+
+    @INLOOP
+    0;JMP
+
+(ROWCONT)
 
     @i
     M=M+1
@@ -73,42 +107,63 @@
     A=M
     D=M
 
-    @INIT
+    @COL //if no key is pressed continues to have the screen blank
     D;JEQ
 
-    @511
-    M=A
-    @255
-    M=A
-
-(COLUMNS)
-    @j
+(COLUMNS) //blacks out the screen if a key is pressed
+    @j //col index
     D=M
-    @511
+    @15
     D=M-D
 
-    @LOOP
-    D;JGT
+    @INIT //returns to key loop once complete
+    D;JLT
 
     @j
     M=M+1
     @i
     M=0
 
-
-(ROWLOOP)
+(ROWLOOP) //nested loop
     @i
     D=M
-    @255
-    D=D-M
+    @15
+    D=M-D
 
     @COLUMNS
-    D;JGT
+    D;JLT
 
     @addr
+    D=M
+
+    @inner
+    M=D
+
+    @b
+    M=0
+(INNERLOOP)
+    @b
+    D=M
+    @31
+    D=M-D
+
+    @ROWCONTINUED
+    D;JLT
+
+    @inner
     A=M
     M=-1
 
+    @inner
+    M=M+1
+
+    @b
+    M=M+1
+
+    @INNERLOOP
+    0;JMP
+
+(ROWCONTINUED)
     @i
     M=M+1
 
